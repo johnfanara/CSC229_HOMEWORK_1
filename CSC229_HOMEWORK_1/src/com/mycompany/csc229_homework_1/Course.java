534a5e35048a1ef2cab0d610c63015d449b0ee66
johnfanara <johnfanara1138@gmail.com>
2022-02-07 19:21:57 -0500

    test commit

---
+++ CSC229_HOMEWORK_1/src/com/mycompany/csc229_homework_1/Course.java
@@ -0,0 +1,45 @@
+package com.mycompany.csc229_homework_1;
+
+public class Course {
+    int ID, code;
+    String name;
+    
+    // default constructor
+    public Course(){
+        ID = 0;
+        name = "";
+        code = 0;
+    }
+    // overloaded constructor
+    public Course(int numID, String userName, int numCode){
+        ID = numID;
+        name = userName;
+        code = numCode;
+    }
+    // setters and getters for all data members
+    public void setID (int numID){
+        ID = numID;
+    }
+    public void setName (String userName) {
+        name = userName;
+    }
+    public void setCode (int numCode) {
+        code = numCode;
+    }
+    public int getID() {
+        return ID;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getCode() {
+        return code;
+    }
+    
+    public static void main (String[] args) {
+        Course course1 = new Course(1138, "John", 42);
+        System.out.println("ID: " + course1.getID());
+        System.out.println("Name: " + course1.getName());
+        System.out.println("Code: " + course1.getCode());
+    }
+}
